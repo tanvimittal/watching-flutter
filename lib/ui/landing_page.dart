d9dbcfa0a7a282fddf2bff00a3f043eabfe683c6
tanvimittal <tanvimittal2k@gmail.com>
2021-06-06 15:57:17 +0900

    - Notification for Android.

--- lib/ui/landing_page.dart
+++ lib/ui/landing_page.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -9,31 +10,54 @@ import 'package:watching_flutter/ui/common_bottom_navigation.dart';
 import 'package:watching_flutter/ui/nickname.dart';
 import 'package:watching_flutter/ui/phone_number.dart';
 
+AndroidNotificationChannel channel = const AndroidNotificationChannel(
+'high_importance_channel', // id
+'High Importance Notifications', // title
+'This channel is used for important notifications.', // description
+importance: Importance.high,
+);
 
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
 Future<void> _messageHandler(RemoteMessage message) async {
   if (Firebase.apps.isEmpty) await Firebase.initializeApp();
-
-  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-  FlutterLocalNotificationsPlugin();
-  const AndroidNotificationDetails androidPlatformChannelSpecifics =
-  AndroidNotificationDetails(
-      'your channel id', 'your channel name', 'your channel description',
-      importance: Importance.max,
-      priority: Priority.high,
-      ticker: 'ticker');
-
-  
-  const NotificationDetails platformChannelSpecifics = NotificationDetails(
-    android: androidPlatformChannelSpecifics,
-  );
-
-  await flutterLocalNotificationsPlugin.show(
-      0, 'plain title', 'plain body', platformChannelSpecifics,
-      payload: 'item x');
+  print('Message recieved');
   print('Handling a background message ${message.messageId}');
-  print(message.data.toString());
+  await showNotification(message);
 }
+
+Future<void> showNotification(RemoteMessage message) async {
+  var eventName;
+  RemoteNotification notification = message.notification;
+  print(notification.hashCode);
+  print(message.data);
+  var userNickname = message.data['user_nickname'];
+  if ("get_up" == message.data['name']) {
+    eventName = userNickname + "さんが起きました。";
+  }
+  else {
+    eventName = userNickname + "さんが寝ました。";
+  }
+  flutterLocalNotificationsPlugin.show(
+      0,
+      '見守りアプリ',
+      eventName,
+      //notification.title,
+      //notification.body,
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          channel.id,
+          channel.name,
+          channel.description,
+          // TODO add a proper drawable resource to android, for now using
+          //      one that already exists in example app.
+          icon: 'launch_background',
+        ),
+      ));
+  // }
+}
+
+
 
 class LandingPage extends StatefulWidget {
   @override
@@ -55,8 +79,8 @@ class _LandingPageState extends State<LandingPage> {
     FirebaseMessaging.onMessageOpenedApp.listen((message) {
       print('Message clicked!');
     });
-
   }
+
   @override
   Widget build(BuildContext context) {
     return _body;
@@ -72,8 +96,12 @@ class _LandingPageState extends State<LandingPage> {
     // Both api_key and nickname are set
     if (prefs.containsKey('api_key') &&
         prefs.containsKey('nickname') &&
-        prefs.getString('api_key').isNotEmpty &&
-        prefs.getString('nickname').isNotEmpty) {
+        prefs
+            .getString('api_key')
+            .isNotEmpty &&
+        prefs
+            .getString('nickname')
+            .isNotEmpty) {
       globals.apiKey = prefs.getString('api_key');
       globals.nickname = prefs.getString('nickname');
       globals.id = prefs.getInt('id');
@@ -83,7 +111,9 @@ class _LandingPageState extends State<LandingPage> {
     // if api_key is set but nickname isn't set
     else if (prefs.containsKey('api_key') &&
         !prefs.containsKey('nickname') &&
-        prefs.getString('api_key').isNotEmpty) {
+        prefs
+            .getString('api_key')
+            .isNotEmpty) {
       globals.apiKey = prefs.getString('api_key');
       globals.id = prefs.getInt('id');
       setState(() => _body = Nickname());
@@ -103,7 +133,7 @@ class _LandingPageState extends State<LandingPage> {
     }
   }
 
-  Future<void> _getUserPermission() async{
+  Future<void> _getUserPermission() async {
     // https://blog.logrocket.com/flutter-push-notifications-with-firebase-cloud-messaging/#buildingtheui
     // On iOS, this helps to take the user permissions
     NotificationSettings settings = await messaging.requestPermission(
@@ -113,29 +143,65 @@ class _LandingPageState extends State<LandingPage> {
       sound: true,
     );
 
-    // https://medium.com/firebase-tips-tricks/how-to-use-firebase-cloud-messaging-in-flutter-a15ca69ff292
-      FirebaseMessaging.onMessage.listen((RemoteMessage event) {
+    if (!kIsWeb) {
+      channel = const AndroidNotificationChannel(
+        'high_importance_channel', // id
+        'High Importance Notifications', // title
+        'This channel is used for important notifications.', // description
+        importance: Importance.high,
+      );
+
+      flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+      await flutterLocalNotificationsPlugin
+          .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+          ?.createNotificationChannel(channel);
+
+      await FirebaseMessaging.instance
+          .setForegroundNotificationPresentationOptions(
+        alert: true,
+        badge: true,
+        sound: true,
+      );
+
+
+      // https://medium.com/firebase-tips-tricks/how-to-use-firebase-cloud-messaging-in-flutter-a15ca69ff292
+      FirebaseMessaging.onMessage.listen((RemoteMessage message) {
         print("message received");
-        if (event.notification!= null) {
-          print(event.notification.body);
-          showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog(
-                  title: Text("notification"),
-                  content: Text("body"),
-                  actions: [
-                    TextButton(onPressed: () {
-                      Navigator.of(context).pop();
-                    },
-                        child: Text("OK"))
-                  ],
-                );
-              });
+        var eventName;
+        RemoteNotification notification = message.notification;
+        print(notification.hashCode);
+        print(message.data);
+        var userNickname = message.data['user_nickname'];
+        if ("get_up" == message.data['name']) {
+          eventName = userNickname + "さんが起きました。";
         }
+        else {
+          eventName = userNickname + "さんが寝ました。";
+        }
+        //AndroidNotification android = message.notification?.android;
+       // if (notification != null && android != null && !kIsWeb) {
+          flutterLocalNotificationsPlugin.show(
+              notification.hashCode,
+              '見守りアプリ',
+              eventName,
+              //notification.title,
+              //notification.body,
+              NotificationDetails(
+                android: AndroidNotificationDetails(
+                  channel.id,
+                  channel.name,
+                  channel.description,
+                  // TODO add a proper drawable resource to android, for now using
+                  //      one that already exists in example app.
+                  icon: 'launch_background',
+                ),
+              ));
+       // }
       });
 
-      messaging.getToken().then((value) => {
+      messaging.getToken().then((value) =>
+      {
         globals.fcmToken = value
       });
 
@@ -150,7 +216,9 @@ class _LandingPageState extends State<LandingPage> {
       fcmStream.listen((token) {
         globals.fcmToken = token;
         Nickname nickname = Nickname();
-        nickname.updateFcmToken(NicknamePost(nickname: globals.nickname, fcmToken: globals.fcmToken));
+        nickname.updateFcmToken(NicknamePost(
+            nickname: globals.nickname, fcmToken: globals.fcmToken));
       });
+    }
   }
 }
