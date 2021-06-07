add4bfdbbe152e216e70067fd82349942e8f94d2
kyuuki <kyuuki.japan@gmail.com>
2021-06-07 17:53:06 +0900

    Remove unnecessary code from main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/material.dart';
-import 'package:watching_flutter/ui/landing_page.dart';
-import 'package:watching_flutter/ui/phone_number.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
+import 'config.dart';
+import 'ui/landing_page.dart';
 
-Future<void> main() async{
+Future<void> main() async {
   // referred Andrea's video for initializing firebase app
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
   runApp(MyApp());
 }
 
@@ -17,28 +17,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: Config.appName,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: LandingPage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    //return PhoneNumber();
-    return LandingPage();
   }
 }
